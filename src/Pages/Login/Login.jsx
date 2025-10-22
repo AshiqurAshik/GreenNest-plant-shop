fd63376e876cd6e6c870bfa93cb1e73932c5386e
Ashiqur Rahman <rashiqur688@gmail.com>
2025-10-23 00:52:01 +0600

    Plant section push

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,16 +1,16 @@
-// import React from 'react';
-// import Header from '../../Components/Header/Header';
-// import Footer from '../../Components/Footer/Footer';
-// import LoginForm from './LoginForm';
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import LoginForm from './LoginForm';
 
-// const Login = () => {
-//   return (
-//     <div>
-//       <Header></Header>
-//       <LoginForm></LoginForm>
-//       <Footer></Footer>
-//     </div>
-//   );
-// };
+const Login = () => {
+  return (
+    <div>
+      <Header></Header>
+      <LoginForm></LoginForm>
+      <Footer></Footer>
+    </div>
+  );
+};
 
-// export default Login;
+export default Login;
